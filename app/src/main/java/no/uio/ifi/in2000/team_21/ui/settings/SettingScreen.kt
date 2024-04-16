69e55ecf41dfa2113c8934fde119eaea0dd14167
Kaja Stenen <kajasten@ulrik.uio.no>
2024-04-16 23:44:50 +0200

    Extended profileImage

--- app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/SettingScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/SettingScreen.kt
@@ -2,16 +2,20 @@ package no.uio.ifi.in2000.team_21.ui.settings
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -23,6 +27,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -42,9 +47,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -541,20 +549,33 @@ fun ProfileImage(){
     val image = rememberSaveable {
         mutableStateOf("")
     }
-    val painter = rememberAsyncImagePainter(
-        if(image.value.isEmpty())
-            R.drawable.user
-        else{
-            image.value
-        }
-    )
+    val painter = if(image.value.isEmpty()){
+        painterResource(id = R.drawable.user)
+    } else{
+        painterResource(id = image.value.toInt())
+    }
+
     Card(
         shape = CircleShape,
+        colors = CardColors(
+            contentColor = profileLight,
+            containerColor = profileLight,
+            disabledContainerColor = profileLight,
+            disabledContentColor = profileLight
+        ),
         modifier = Modifier
-            .padding(8.dp)
             .size(60.dp)
-        ){
-
+    ){
+        Image(painter = painter,
+            contentDescription = null,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(15.dp)
+                .size(30.dp)
+                .clip(CircleShape)
+                .clickable { },
+            contentScale = ContentScale.Crop
+        )
     }
 }
 

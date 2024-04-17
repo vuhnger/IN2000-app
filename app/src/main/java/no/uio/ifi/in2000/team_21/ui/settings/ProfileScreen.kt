6fd068c2155c583d1121d43fa4f66265c50459de
Mari Stenbrenden <mastenb@uio.no>
2024-04-17 11:00:34 +0200

    Added top bar in profile screen, and the skeleton to the screen

--- app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/ProfileScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/ProfileScreen.kt
@@ -1,2 +1,81 @@
 package no.uio.ifi.in2000.team_21.ui.settings
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import no.uio.ifi.in2000.team_21.Screen
+import no.uio.ifi.in2000.team_21.ui.theme.backgroundLight
+import no.uio.ifi.in2000.team_21.ui.theme.onContainerLight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen (navController: NavController) {
+
+    val FUNCTION_NAME = object {}.javaClass.enclosingMethod.name
+
+    Log.d(
+        FUNCTION_NAME,
+        "called"
+    )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Profil",
+                        color = onContainerLight
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+                        Icon(
+                            contentDescription = "Tilbake",
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                            tint = onContainerLight,
+                            modifier = Modifier
+                                .size(30.dp)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(backgroundLight)
+            )
+        },
+        containerColor = backgroundLight
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ){
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ProfileScreenTest() {
+    ProfileScreen(navController = rememberNavController())
+}

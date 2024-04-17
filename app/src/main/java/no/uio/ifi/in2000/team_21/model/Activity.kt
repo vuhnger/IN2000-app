d1431152f1ca71c87585d2acea150c450610b947
vuhger <victor.uhnger@gmail.com>
2024-04-17 19:46:49 +0200

    Revert "change activity model"
    This reverts commit 6cd08e95f243ed7d68ddc0caecaaed4812455326.
    Denne fucka også opp alt

--- app/src/main/java/no/uio/ifi/in2000/team_21/model/Activity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_21/model/Activity.kt
@@ -1,20 +1,16 @@
 package no.uio.ifi.in2000.team_21.model
 
-
-// Egenskapene til klassen(e) skal tilsvare navn på variabler vi får fra api.met.no
 abstract class Activity{
 
     abstract val type: String
-    abstract val description: String
     abstract val air_temperature: Double?
     abstract val air_temperature_unit: String?
     abstract val sea_water_temperature: Double?
     abstract val sea_water_temperature_unit: String?
     abstract val sea_water_speed: Double? // Current
     abstract val sea_surface_wave_height: Double?
-    abstract val imageResource: Resource?
+    abstract val imageResource: Resource
     abstract val icons: List<String>?
-
 }
 
 class Kayaking(
@@ -26,8 +22,7 @@ class Kayaking(
     override val sea_water_speed: Double,
     override val sea_surface_wave_height: Double,
     override val imageResource: Resource,
-    override val icons: List<String>,
-    override val description: String
+    override val icons: List<String>
 ) : Activity() {
 
     val sea_water_to_direction: Double? = null

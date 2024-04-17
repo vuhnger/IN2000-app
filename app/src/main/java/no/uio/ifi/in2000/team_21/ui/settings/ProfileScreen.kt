c957017c05e57eab204061e6885dcfd94dabc5bd
Mari Stenbrenden <mastenb@uio.no>
2024-04-17 18:01:18 +0200

    test

--- app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/ProfileScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_21/ui/settings/ProfileScreen.kt
@@ -30,8 +30,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -55,6 +57,8 @@ fun ProfileScreen (navController: NavController) {
     val FUNCTION_NAME = object {}.javaClass.enclosingMethod.name
     
     val notification = rememberSaveable { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("Navn") }
+    var username by rememberSaveable { mutableStateOf("Brukernavn") }
 
     if (notification.value.isNotEmpty()) {
         Toast.makeText(LocalContext.current, notification.value, Toast.LENGTH_LONG).show()

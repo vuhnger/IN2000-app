eac6297a836e70230b610eff1c35377e34b7663a
harepus <sebastian.s.hareide@gmail.com>
2024-05-13 13:04:53 +0200

    added splashscreen to mainactivity, and the png file of the splashscreen made in figma

--- app/src/main/java/no/uio/ifi/in2000/team_21/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team_21/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -226,7 +229,7 @@ fun SplashScreen(navController: NavController) {
     // AnimationEffect
     LaunchedEffect(key1 = true) {
         scale.animateTo(
-            targetValue = 1.2f,
+            targetValue = 1.0f,
             animationSpec = tween(
                 durationMillis = 800,
                 easing = {
@@ -239,9 +242,9 @@ fun SplashScreen(navController: NavController) {
 
     // Image
     Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()) {
-        Image(painter = painterResource(id = R.drawable.splashscreen_1080_2340),
+        modifier = Modifier.fillMaxWidth().background(color = Color(0xFFE7F3FC))) {
+        Image(painter = painterResource(id = R.drawable.prototype78),
             contentDescription = "Logo",
-            modifier = Modifier.scale(scale.value))
+            modifier = Modifier.scale(scale.value).fillMaxWidth())
     }
 }
